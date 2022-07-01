d63e638588e784028895bfb5431045f4b1397918
shreeja1128 <83698103+shreeja1128@users.noreply.github.com>
2022-07-01 14:58:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,194 @@
+import tkinter as tk
+from tkinter import *
+import cv2
+from PIL import Image, ImageTk
+import os
+import numpy as np
+import cv2
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+emotion_model = Sequential()
+
+emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Flatten())
+emotion_model.add(Dense(1024, activation='relu'))
+emotion_model.add(Dropout(0.5))
+emotion_model.add(Dense(7, activation='softmax'))
+
+
+cv2.ocl.setUseOpenCL(False)
+
+emotion_dict = {0: "   Angry   ", 1: "Disgusted", 2: "  Fearful  ", 3: "   Happy   ", 4: "  Neutral  ", 5: "    Sad    ", 6: "Surprised"}
+
+
+emoji_dist={0:"./emojis/angry.png",1:"./emojis/disgusted.png",2:"./emojis/fearful.png",3:"./emojis/happy.png",4:"./emojis/neutral.png",5:"./emojis/sad.png",6:"./emojis/surprised.png"}
+
+show_text=[0]
+global prevImg
+
+fileName = os.environ['ALLUSERSPROFILE'] + "\WebcamCap.txt"
+cancel = False
+
+def prompt_ok(event = 0):
+    global cancel, button, button1, button2
+    cancel = True
+
+    button.place_forget()
+    button1 = tk.Button(mainWindow, text="Convert!", command=saveAndExit)
+    button2 = tk.Button(mainWindow, text="Try Again", command=resume)
+    button1.place(anchor=tk.CENTER, relx=0.2, rely=0.9, width=150, height=50)
+    button2.place(anchor=tk.CENTER, relx=0.8, rely=0.9, width=150, height=50)
+    button1.focus()
+
+def saveAndExit(event = 0):
+
+    if (len(sys.argv) < 2):
+        filepath = "imageCap.png"
+    else:
+        filepath = sys.argv[1]
+
+    prevImg.save(filepath)
+    pre_emoji()
+    mainWindow.quit()
+
+def pre_emoji():
+    global last_frame1
+    last_frame1 = np.zeros((480, 640, 3), dtype=np.uint8)
+    root=tk.Toplevel()
+    last_frame1= frame.copy()
+
+    bounding_box = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    gray_frame = cv2.cvtColor(last_frame1, cv2.COLOR_BGR2GRAY)
+    num_faces = bounding_box.detectMultiScale(gray_frame,scaleFactor=1.3, minNeighbors=5)
+
+    for (x, y, w, h) in num_faces:
+        cv2.rectangle(last_frame1, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+        roi_gray_frame = gray_frame[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+        prediction = emotion_model.predict(cropped_img)
+
+        maxindex = int(np.argmax(prediction))
+        cv2.putText(last_frame1, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        show_text[0]=maxindex
+
+    img = ImageTk.PhotoImage(Image.open("logo.png"))
+    heading = Label(root,image=img,bg='black')
+    heading.pack()
+
+
+    lmain = tk.Label(master=root,padx=50,bd=10)
+    lmain2 = tk.Label(master=root,bd=10)
+
+    lmain3=tk.Label(master=root,bd=10,fg="#CDCDCD",bg='black')
+    lmain.pack(side=LEFT)
+    lmain.place(x=30,y=150)
+    lmain3.pack()
+    lmain3.place(x=960,y=100)
+    lmain2.pack(side=RIGHT)
+    lmain2.place(x=900,y=150)
+
+    root.title("EMOJIFY")
+    root.geometry("1400x850")
+    root['bg']='black'
+
+    last_frame1 = cv2.resize(last_frame1,(600,500))
+    pic = cv2.cvtColor(last_frame1, cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(pic)
+    imgtk = ImageTk.PhotoImage(image=img)
+    lmain.imgtk = imgtk
+    lmain.configure(image=imgtk)
+
+    frame2=cv2.imread(emoji_dist[show_text[0]])
+    pic2=cv2.cvtColor(frame2,cv2.COLOR_BGR2RGB)
+    img2=Image.fromarray(frame2)
+    imgtk2=ImageTk.PhotoImage(image=img2)
+    lmain2.imgtk2=imgtk2
+    lmain3.configure(text=emotion_dict[show_text[0]],font=('arial',45,'bold'))
+    lmain2.configure(image=imgtk2)
+
+    root.mainloop()
+
+
+def resume(event = 0):
+    global button1, button2, button, lmain, cancel
+
+    cancel = False
+
+    button1.place_forget()
+    button2.place_forget()
+
+    mainWindow.bind('<Return>', prompt_ok)
+    button.place(bordermode=tk.INSIDE, relx=0.5, rely=0.9, anchor=tk.CENTER, width=300, height=50)
+    lmain.after(10, show_frame)
+
+
+try:
+    f = open(fileName, 'r')
+    camIndex = int(f.readline())
+except:
+    camIndex = 0
+
+cap = cv2.VideoCapture(camIndex)
+capWidth = cap.get(3)
+capHeight = cap.get(4)
+
+success, frame = cap.read()
+if not success:
+    if camIndex == 0:
+        print("Error, No webcam found!")
+        sys.exit(1)
+    else:
+        changeCam(nextCam=0)
+        success, frame = cap.read()
+        if not success:
+            print("Error, No webcam found!")
+            sys.exit(1)
+
+
+mainWindow = tk.Tk()
+mainWindow.title('Photo Capture')
+mainWindow.resizable(width=False, height=False)
+mainWindow.bind('<Escape>', lambda e: mainWindow.quit())
+lmain = tk.Label(mainWindow, compound=tk.CENTER, anchor=tk.CENTER, relief=tk.RAISED)
+button = tk.Button(mainWindow, text="Capture", command=prompt_ok)
+lmain.pack()
+button.place(bordermode=tk.INSIDE, relx=0.5, rely=0.9, anchor=tk.CENTER, width=300, height=50)
+button.focus()
+
+
+def show_frame():
+    global cancel, prevImg, button,frame
+
+    _, frame = cap.read()
+
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+
+    prevImg = Image.fromarray(cv2image)
+    imgtk = ImageTk.PhotoImage(image=prevImg)
+    lmain.imgtk = imgtk
+    lmain.configure(image=imgtk)
+    if not cancel:
+        lmain.after(10, show_frame)
+
+show_frame()
+mainWindow.mainloop()
